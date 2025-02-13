f4c2aecf22257e9d982f5262417584ad033f2b2b
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-13 19:25:35 -0300

    feat: :sparkles: add: feature flag para /eventos e testes de /eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -2,12 +2,12 @@ import Evento from '../models/evento.js';
 
 class EventosController {
   static liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
-  
+
   static listarEventos = async (req, res) => {
     if (this.liberaAcessoEventos()) {
       try {
         const resultado = await Evento.pegarEventos();
-        return res.status(200).json(resultado);
+        return res.status(200).json(`Resultado: ${resultado}`);
       } catch (err) {
         return res.status(500).json(err.message);
       }
